d810b9f8dfb6ceb438ed138e9e5764926795b277
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-04 22:25:45 -0700

    Created the index page for authors

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '/books', to: 'books#index'
+  get '/authors', to: 'authors#index'
+  # get '/authors/:author_id/books', to: 'author_books#index'
 end
---
+++ spec/features/authors/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Artists songs index' do
+  
+  it 'shows all of the titles of the songs for the artist' do
+    muir = Author.create!(name: 'Tamsyn Muir', alive: true, hometown: 'Howick, New Zealand', age_published: 34)
+    gideon = muir.books.create!(title: 'Gideon the Ninth', publish_date: '2019-09-10', pages: 448, in_series: true)
+    harrow = muir.books.create!(title: 'Harrow the Ninth', publish_date: '2020-08-04', pages: 552, in_series: true)
+    nona = muir.books.create!(title: 'Nona the Ninth', publish_date: '2022-09-13', pages: 480, in_series: true)
+    flora = muir.books.create!(title: 'Princess Floralinda and the Forty-Flight Tower', publish_date: '2020-11-30', pages: 146, in_series: false)
+    
+    visit "/books"
+    
+    expect(page).to have_content(muir.name)
+    expect(page).to have_content(muir.books)
+  end
+end
