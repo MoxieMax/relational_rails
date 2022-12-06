0e4673816d2b331375adf1579e7d052a63effbea
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-05 22:29:36 -0700

    Completed User Story 6

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :books
+  
+  def self.order_created
+    order(created_at: :desc)
+  end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  it {should have_many :books}
+  describe 'relationships' do
+    it {should have_many :books}
+  end
 end
