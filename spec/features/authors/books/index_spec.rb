f35c06b1b79a55f0d84b74d7f5eeb85a9a48b833
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-06 15:50:02 -0700

    Updated authors/books/index_spec to include a before block for test variables

--- spec/features/authors/books/index_spec.rb
+++ spec/features/authors/books/index_spec.rb
@@ -2,33 +2,36 @@ require 'rails_helper'
 
 RSpec.describe 'Authors books index' do
   
+  before do
+    @muir = Author.create!(name: 'Tamsyn Muir', alive: true, hometown: 'Howick, New Zealand', age_published: 34)
+    @gideon = @muir.books.create!(title: 'Gideon the Ninth', publish_date: '2019-09-10', pages: 448, in_series: true)
+    @harrow = @muir.books.create!(title: 'Harrow the Ninth', publish_date: '2020-08-04', pages: 552, in_series: true)
+    @nona = @muir.books.create!(title: 'Nona the Ninth', publish_date: '2022-09-13', pages: 480, in_series: true)
+    @flora = @muir.books.create!(title: 'Princess Floralinda and the Forty-Flight Tower', publish_date: '2020-11-30', pages: 146, in_series: false)
+  end
+  
   it 'shows all of the titles of the books for the author' do
-    muir = Author.create!(name: 'Tamsyn Muir', alive: true, hometown: 'Howick, New Zealand', age_published: 34)
-    gideon = muir.books.create!(title: 'Gideon the Ninth', publish_date: '2019-09-10', pages: 448, in_series: true)
-    harrow = muir.books.create!(title: 'Harrow the Ninth', publish_date: '2020-08-04', pages: 552, in_series: true)
-    nona = muir.books.create!(title: 'Nona the Ninth', publish_date: '2022-09-13', pages: 480, in_series: true)
-    flora = muir.books.create!(title: 'Princess Floralinda and the Forty-Flight Tower', publish_date: '2020-11-30', pages: 146, in_series: false)
     
-    visit "/authors/#{muir.id}/books"
+    visit "/authors/#{@muir.id}/books"
     
-    expect(page).to have_content(gideon.title)
-    expect(page).to have_content(harrow.title)
-    expect(page).to have_content(nona.title)
-    expect(page).to have_content(flora.title)
+    expect(page).to have_content(@gideon.title)
+    expect(page).to have_content(@harrow.title)
+    expect(page).to have_content(@nona.title)
+    expect(page).to have_content(@flora.title)
     
-    expect(page).to have_content(gideon.publish_date)
-    expect(page).to have_content(harrow.publish_date)
-    expect(page).to have_content(nona.publish_date)
-    expect(page).to have_content(flora.publish_date)
+    expect(page).to have_content(@gideon.publish_date)
+    expect(page).to have_content(@harrow.publish_date)
+    expect(page).to have_content(@nona.publish_date)
+    expect(page).to have_content(@flora.publish_date)
     
-    expect(page).to have_content(gideon.pages)
-    expect(page).to have_content(harrow.pages)
-    expect(page).to have_content(nona.pages)
-    expect(page).to have_content(flora.pages)
+    expect(page).to have_content(@gideon.pages)
+    expect(page).to have_content(@harrow.pages)
+    expect(page).to have_content(@nona.pages)
+    expect(page).to have_content(@flora.pages)
     
-    expect(page).to have_content(gideon.in_series)
-    expect(page).to have_content(harrow.in_series)
-    expect(page).to have_content(nona.in_series)
-    expect(page).to have_content(flora.in_series)
+    expect(page).to have_content(@gideon.in_series)
+    expect(page).to have_content(@harrow.in_series)
+    expect(page).to have_content(@nona.in_series)
+    expect(page).to have_content(@flora.in_series)
   end
 end
