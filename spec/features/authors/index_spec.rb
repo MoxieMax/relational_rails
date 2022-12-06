5b21d782d772d14ade2d991f9d205e710c65b89c
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-05 22:34:32 -0700

    Updated the authors/show_spec to have a before block

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe 'Authors index' do
     @muir = Author.create!(name: 'Tamsyn Muir', alive: true, hometown: 'Howick, New Zealand', age_published: 34)
     @king = Author.create!(name: 'Stephen King', alive: true, hometown: 'Portland, Maine, USA', age_published: 26)
     @chupeco = Author.create!(name: 'Rin Chupeco', alive: true, hometown: 'Manila, Philippines', age_published: 30)
+    
     visit "/authors"
+    
     save_and_open_page
   end
   
--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Authors show' do
+  before do
+    @author = Author.create!(name: 'Tamsyn Muir', alive: true, hometown: 'Howick, New Zealand', age_published: 34)
+    
+    visit "/authors/#{@author.id}"
+  end
   
   it 'shows all authors' do
-    author = Author.create!(name: 'Tamsyn Muir', alive: true, hometown: 'Howick, New Zealand', age_published: 34)
-    
-    visit "/authors/#{author.id}"
-    expect(page).to have_content(author.name)
-    expect(page).to have_content(author.alive)
-    expect(page).to have_content(author.hometown)
-    expect(page).to have_content(author.age_published)
+    expect(page).to have_content(@author.name)
+    expect(page).to have_content(@author.alive)
+    expect(page).to have_content(@author.hometown)
+    expect(page).to have_content(@author.age_published)
   end
+  
+  # xit '' do
+  # 
+  # end
 end
