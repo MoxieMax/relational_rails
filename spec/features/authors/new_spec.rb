cfe06fdb939b60eedef75ab79688020457f8f4c8
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-06 14:30:12 -0700

    Created the /authors/new page, the test in authors/new_spec and a test in authors/index_spec to verify links function

--- spec/features/authors/new_spec.rb
+++ spec/features/authors/new_spec.rb
@@ -8,15 +8,16 @@ RSpec.describe 'the Author creation' do
     expect(current_path).to eq('/authors/new')
   end
 
-  xit 'can create a new author' do
+  it 'can create a new author' do
     visit '/authors/new'
-
-    fill_in('Name', with: 'Richelle Mead')
-    fill_in('Alive', with: true)
-    fill_in('Hometown', with: 'Kirkland, WA')
-    fill_in('Age First Published', with: 31)
+    
+    fill_in 'Name', with: 'Richelle Mead'
+    check 'Alive'
+    fill_in 'Hometown', with: 'Kirkland, WA'
+    fill_in 'age_published', with: 31
+    
     click_button('Create Author')
-
+    
     expect(current_path).to eq('/authors')
     expect(page).to have_content('Richelle Mead')
   end
